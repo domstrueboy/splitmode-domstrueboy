444c003dbb40188b2e354f4c06536b23b5e081a6
domstrueboy <domstrueboy@rambler.ru>
2015-11-17 01:47:05 +0500

    Normal game, without AI. UI can also be improved.

--- script.js
+++ script.js
@@ -3,46 +3,108 @@
     var app = {
 
         init: function(){
-            app.resetBoard();
-            app.step = 1;
-            app.state = app.stateUpdate(app.step);
-            app.clickControl();
+          this.newGame();
+          this.appBuild();
         },
 
-        stepUpdate: function(step){
-          return ++step;
+        appBuild: function(){
+          this.menuControl();
+          this.clickControl();
         },
 
-		stateUpdate: function(step){
-			if(step % 2 !== 0){
-				return "zero";
-			} else{
-				return "cross";
-			}
-		},
+        nextGame: function(){
+          this.resetBoard();
+          this.step = 1;
+          this.state = this.prevWinner;
+          this.displayCurrentPlayer();
+        },
+
+        newGame: function(){
+          this.resetBoard();
+          this.step = 1;
+          this.state = "zero";
+          this.displayCurrentPlayer();
+          this.scoreZero = 0;
+          this.scoreCross = 0;
+          this.scoreUpdate();
+
+        },
+
+        nextStep: function(){
+          return ++this.step;
+        },
+
+    		stateUpdate: function(){
+    			if(this.state === "cross"){
+    				return "zero";
+    			} else if(this.state === "zero"){
+    				return "cross";
+    			}
+    		},
+
+        playerWin: function(classDetected){
+
+          if(classDetected === "zero"){
+    				app.scoreZero += 2;
+    			} else if(classDetected === "cross"){
+    				app.scoreCross += 2;
+    			}
+
+          app.scoreUpdate();
+        },
+
+        friendshipWin: function(){
+    				app.scoreZero++;
+    				app.scoreCross++;
+
+            app.scoreUpdate();
+        },
+
+        scoreUpdate: function(){
+          $(".score-playerZero").text("" + app.scoreZero);
+          $(".score-playerCross").text("" + app.scoreCross);
+        },
+
+        menuControl: function(){
+          $(".newGame").click(function(){
+            app.newGame();
+          });
+        },
+
+        displayCurrentPlayer: function(){
+          if(app.state === "zero"){
+            $(".score-cell-cross").removeClass("score-cross");
+            $(".score-cell-zero").addClass("score-zero");
+          } else if(app.state === "cross"){
+            $(".score-cell-zero").removeClass("score-zero");
+            $(".score-cell-cross").addClass("score-cross");
+          }
+        },
 
         clickControl: function(){
 
-			$(".cell").click(function(){
+			       $(".cell").click(function(){
 
-            if(!$(this).hasClass("zero") && !$(this).hasClass("cross")){
+                if(!$(this).hasClass("zero") && !$(this).hasClass("cross")){
 
-              $(this).addClass(app.state);
-              app.step = app.stepUpdate(app.step);
-              app.state = app.stateUpdate(app.step);
+                  $(this).addClass(app.state);
+                  app.step = app.nextStep();
+                  app.state = app.stateUpdate();
+                  app.displayCurrentPlayer();
 
-              if(app.step > 5){
-                app.detectWin("zero");
-                app.detectWin("cross");
-              }
+                  if(app.step > 5){
+                    app.detectWin("zero");
+                    app.detectWin("cross");
+                  }
 
-              if(app.step > 9){
-                alert("Frienship win!");
-                app.init();
-              }
-            }
-					})
-        },
+                  if(app.step > 9){
+                    alert("Friendship win!");
+                    app.friendshipWin();
+                    app.nextGame();
+                  }
+                }
+    					})
+            },
 
         resetBoard: function(){
           $(".cell").removeClass("zero").removeClass("cross");
@@ -83,8 +145,10 @@
              $(".row2:eq(1)").hasClass(classDetected) &&
              $(".row3:eq(0)").hasClass(classDetected))
            ){
-            alert(classDetected.charAt(0).toUpperCase() + classDetected.substr(1) + " WIN!");
-            app.init();
+             app.prevWinner = classDetected;
+             app.playerWin(classDetected);
+             alert(classDetected.charAt(0).toUpperCase() + classDetected.substr(1) + " WIN!");
+             app.nextGame();
           }
 
         }
